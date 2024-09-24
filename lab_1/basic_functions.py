91f31b171b87af8a38d2f79f02ab37f11ca69093
Kl1rik <kirillshershan@gmail.com>
2024-09-24 23:11:37 +0300

    add global variables

--- lab_1/basic_functions.py
+++ lab_1/basic_functions.py
@@ -1,6 +1,30 @@
 import subprocess
 import os
 from colorama import Fore,Back,Style
+user = "iit11"
+files_path_user = f"/home/{user}/pzs/pzs11"
+files_path_group = f"/home/{user}/pzs/pzs12"
+files_path_others = f"/home/{user}/pzs/pzs13"
+files_path_all = f"/home/{user}/pzs/pzs14"
+files_path_root = f"/home/{user}/pzs/pzs15"
+
+folder_list = [files_path_user ,files_path_group , files_path_others , files_path_all , files_path_root]
+
+files_path_group = files_path_user
+files_path_others =files_path_user
+files_path_all = files_path_user
+files_path_root = files_path_user
+
+
+files_user = [f'{files_path_user}/file11',f'{files_path_user}/file12',f'{files_path_user}/file13',f'{files_path_user}/file14',f'{files_path_user}/file15']
+files_group = [f'{files_path_group}/file21',f'{files_path_group}/file22',f'{files_path_group}/file23',f'{files_path_group}/file24',f'{files_path_group}/file25']
+files_others = [f'{files_path_others}/file31',f'{files_path_others}/file32',f'{files_path_others}/file33',f'{files_path_others}/file34',f'{files_path_others}/file35']
+files_all = [f'{files_path_all}/file41',f'{files_path_all}/file42',f'{files_path_all}/file43',f'{files_path_all}/file44',f'{files_path_all}/file45']
+files_root = [f'{files_path_root}/file51',f'{files_path_root}/file52',f'{files_path_root}/file53',f'{files_path_root}/file54',f'{files_path_root}/file55']
+
+files_test_variable_list = files_user + files_group + files_others + files_all
+files_execute_list = files_user[-1] + files_group[-1] + files_others[-1] + files_all[-1] + files_root[-1]
+
 def run_command(command):
     try:
         result = subprocess.run(command, shell=True, check=True ,stdout=subprocess.DEVNULL ,stderr=subprocess.DEVNULL)
@@ -42,34 +66,8 @@ def stop_process(pid, user):
         print(f"Ошибка при проверке возможности остановки процесса с PID {pid} пользователем {user}: {e}")
 
 def crud_txt_file():
-    user = "iit11"
     user_list = ['iit11', 'iit12', 'iit21', 'iit22', 'iit3','root']
     # parse_output(subprocess.check_output("whoami"))
-    files_path_user = f"/home/{user}/pzs/pzs11"
-    files_path_group = f"/home/{user}/pzs/pzs12"
-    files_path_others = f"/home/{user}/pzs/pzs13"
-    files_path_all = f"/home/{user}/pzs/pzs14"
-    files_path_root = f"/home/{user}/pzs/pzs15"
-
-    folder_list = [files_path_user ,files_path_group , files_path_others , files_path_all , files_path_root]
-
-    files_path_group = files_path_user
-    files_path_others =files_path_user
-    files_path_all = files_path_user
-    files_path_root = files_path_user
-
-
-    files_user = [f'{files_path_user}/file11',f'{files_path_user}/file12',f'{files_path_user}/file13',f'{files_path_user}/file14',f'{files_path_user}/file15']
-    files_group = [f'{files_path_group}/file21',f'{files_path_group}/file22',f'{files_path_group}/file23',f'{files_path_group}/file24',f'{files_path_group}/file25']
-    files_others = [f'{files_path_others}/file31',f'{files_path_others}/file32',f'{files_path_others}/file33',f'{files_path_others}/file34',f'{files_path_others}/file35']
-    files_all = [f'{files_path_all}/file41',f'{files_path_all}/file42',f'{files_path_all}/file43',f'{files_path_all}/file44',f'{files_path_all}/file45']
-    files_root = [f'{files_path_root}/file51',f'{files_path_root}/file52',f'{files_path_root}/file53',f'{files_path_root}/file54',f'{files_path_root}/file55']
-    
-    files_test_variable_list = files_user + files_group + files_others + files_all
-    files_execute_list = files_user[-1] + files_group[-1] + files_others[-1] + files_all[-1] + files_root[-1]
-
-
-        
     print(Fore.RED + "Начало проверки файлов")
 
     for file in files_test_variable_list:
@@ -87,8 +85,6 @@ def crud_txt_file():
         print("Да" if execute else "Нет")
         
     
-                
-
 
     print(Fore.MAGENTA + "Начало проверки директорий")
     
@@ -111,31 +107,8 @@ def crud_txt_file():
         print("Да" if execute_dir else "Нет")
 
 
-
 def custom_user_run(program):
     user = "iit11"
-    files_path_user = f"/home/{user}/pzs/pzs11"
-    files_path_group = f"/home/{user}/pzs/pzs12"
-    files_path_others = f"/home/{user}/pzs/pzs13"
-    files_path_all = f"/home/{user}/pzs/pzs14"
-    files_path_root = f"/home/{user}/pzs/pzs15"
-
-    folder_list = [files_path_user ,files_path_group , files_path_others , files_path_all , files_path_root]
-
-    files_path_group = files_path_user
-    files_path_others =files_path_user
-    files_path_all = files_path_user
-    files_path_root = files_path_user
-
-
-    files_user = [f'{files_path_user}/file11',f'{files_path_user}/file12',f'{files_path_user}/file13',f'{files_path_user}/file14',f'{files_path_user}/file15']
-    files_group = [f'{files_path_group}/file21',f'{files_path_group}/file22',f'{files_path_group}/file23',f'{files_path_group}/file24',f'{files_path_group}/file25']
-    files_others = [f'{files_path_others}/file31',f'{files_path_others}/file32',f'{files_path_others}/file33',f'{files_path_others}/file34',f'{files_path_others}/file35']
-    files_all = [f'{files_path_all}/file41',f'{files_path_all}/file42',f'{files_path_all}/file43',f'{files_path_all}/file44',f'{files_path_all}/file45']
-    files_root = [f'{files_path_root}/file51',f'{files_path_root}/file52',f'{files_path_root}/file53',f'{files_path_root}/file54',f'{files_path_root}/file55']
-    
-    files_test_variable_list = files_user + files_group + files_others + files_all
-    files_execute_list = files_user[-1] + files_group[-1] + files_others[-1] + files_all[-1] + files_root[-1]
 
     program_path = parse_output(subprocess.check_output("pwd"))
     program_full_path = program_path + program

f8f663607750d8ee850dcafe2ed1fa54b8e06e42
Kl1rik <kirillshershan@gmail.com>
2024-09-24 22:40:16 +0300

    add write try function

--- lab_1/basic_functions.py
+++ lab_1/basic_functions.py
@@ -58,6 +58,8 @@ def crud_txt_file():
         print("Да" if read else "Нет")
 
         # print(Fore.WHITE + f"Редактирование файла {file}")
+        write_dir = os.access(file, os.W_OK)
+        print("Да" if write else "Нет")
 
         # print(Fore.WHITE + f"Удаление файла {file}")
         execute = os.access(file, os.X_OK)
